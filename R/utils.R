7596220aa1067ad748453f8796a5515658377442
Ahmadou Dicko <mail@ahmadoudicko.com>
2021-03-03 16:41:09 +0000

    Add more informative message and no need to run pd_session

--- R/utils.R
+++ R/utils.R
@@ -5,17 +5,22 @@ assert_cache <- function(x)
 
 #' @noRd
 #' @importFrom crul HttpClient auth
-pd_GET <- function(path, cookie = Sys.getenv("POPDATA_COOKIE", "")) {
+pd_GET <- function(path, cookie = Sys.getenv("POPDATA_COOKIE", ""), verbose = TRUE) {
   cli <- HttpClient$new(url = "https://popdata.unhcr.org",
                         opts = list(cookie = cookie))
   res <- cli$get(path)
   res$raise_for_status()
-  res_content_type <- res$response_headers[["content-type"]]
-  bool <- grepl("text/csv", res_content_type, ignore.case = TRUE)
-  if (!bool) {
-    pd_session()
-    res <- cli$get(path)
-    res$raise_for_status()
-  }
+  res <- tryCatch({res$raise_for_ct(type = "text/csv"); res},
+                  error = function(e) {
+                    if (verbose)
+                      message("Your session has expired, generating a new cookie!")
+                    pd_session()
+                    cookie <- Sys.getenv("POPDATA_COOKIE", "")
+                    cli <- HttpClient$new(url = "https://popdata.unhcr.org",
+                                          opts = list(cookie = cookie))
+                    res <- cli$get(path)
+                    res$raise_for_status()
+                    res
+                  })
   res
 }
