33ae431ef8633e0a02378c43da53bad7d6b3c353
Hisham Galal <hkotri@gmail.com>
2021-08-24 19:10:55 -0500

    Add VDA and HC tables to MYSR

--- R/popdata.R
+++ R/popdata.R
@@ -43,7 +43,7 @@ pd_asr <- function(table = c("refugees", "refugeelike", "refugeeLike",
 #' @rdname popdata
 #' @export
 pd_mysr <- function(table = c("refugees", "returnees", "idp",
-                              "rsd", "stateless", "other"),
+                              "rsd", "stateless", "other", "vda", "hostCommunity"),
                     year = 2020,
                     quiet = getOption("popdata_quiet")) {
   table <- match.arg(table)
